06c641c7140e76a5bad84cfd03ae5b4258933374
Elijah Alonzo <elijahalonzo.me@gmail.com>
2026-01-29 04:48:26 +0800

    add grading-sys to project section

--- app/components/projects-section.tsx
+++ app/components/projects-section.tsx
@@ -34,7 +34,67 @@ export default function ProjectsSection() {
                 showcase, and rank student portfolios efficiently.
               </p>
               <a
-                href="https://github.com/elijah-alonzo/digital-twin.git"
+                href="https://github.com/elijah-alonzo/ranking-sys.git"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-2 text-blue-500 hover:text-blue-600 text-sm"
+              >
+                <Github size={16} />
+                View on GitHub
+              </a>
+            </div>
+          </div>
+
+          {/* Grading Sheet Monitoring System */}
+          <div className="border border-gray rounded-xl overflow-hidden shadow-lg">
+            <div className="h-36 overflow-hidden relative">
+              <img
+                src="/images/logo-grading-sys.png"
+                alt="Grading Sheet Monitoring System"
+                className="w-full h-full object-contain"
+              />
+              <div className="absolute inset-0"></div>
+            </div>
+            <div className="p-6">
+              <h3 className="text-l font-semibold text-gray-900 mb-2">
+                Grading Sheet Monitoring System
+              </h3>
+              <p className="text-gray-600 mb-4 text-sm">
+                A web app for the university registrar and staff to track and
+                monitor submission of grading sheets.
+              </p>
+              <a
+                href="https://github.com/elijah-alonzo/grading-sys.git"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-2 text-blue-500 hover:text-blue-600 text-sm"
+              >
+                <Github size={16} />
+                View on GitHub
+              </a>
+            </div>
+          </div>
+
+          {/* Equal Learn */}
+          <div className="border border-gray rounded-xl overflow-hidden shadow-lg">
+            <div className="h-36 overflow-hidden relative">
+              <img
+                src="/images/logo-equallearn.png"
+                alt="e-Portfolio and Ranking System"
+                className="w-full h-full object-contain"
+              />
+              <div className="absolute inset-0"></div>
+            </div>
+            <div className="p-6">
+              <h3 className="text-l font-semibold text-gray-900 mb-2">
+                Equal Learn
+              </h3>
+              <p className="text-gray-600 mb-4 text-sm">
+                A platform for the Paulinian Student Government to manage,
+                showcase, and rank student portfolios efficiently.
+              </p>
+              <a
+                href="https://github.com/elijah-alonzo/ranking-sys.git"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center gap-2 text-blue-500 hover:text-blue-600 text-sm"
