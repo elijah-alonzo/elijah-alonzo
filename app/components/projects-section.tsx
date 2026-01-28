83c3675a482dce204becd353f802e0ee95683a3f
Elijah Alonzo <elijahalonzo.me@gmail.com>
2026-01-29 04:55:48 +0800

    add lucky llama and equal learn to project section

--- app/components/projects-section.tsx
+++ app/components/projects-section.tsx
@@ -80,7 +80,7 @@ export default function ProjectsSection() {
             <div className="h-36 overflow-hidden relative">
               <img
                 src="/images/logo-equallearn.png"
-                alt="e-Portfolio and Ranking System"
+                alt="Equal Learn"
                 className="w-full h-full object-contain"
               />
               <div className="absolute inset-0"></div>
@@ -90,11 +90,41 @@ export default function ProjectsSection() {
                 Equal Learn
               </h3>
               <p className="text-gray-600 mb-4 text-sm">
-                A platform for the Paulinian Student Government to manage,
-                showcase, and rank student portfolios efficiently.
+                A video hosting platform developed as the final output for the
+                Collaborative Online International Learning (COIL) project.
               </p>
               <a
-                href="https://github.com/elijah-alonzo/ranking-sys.git"
+                href="https://github.com/elijah-alonzo/equal-learn.git"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-2 text-blue-500 hover:text-blue-600 text-sm"
+              >
+                <Github size={16} />
+                View on GitHub
+              </a>
+            </div>
+          </div>
+
+          {/* Llucky Llama */}
+          <div className="border border-gray rounded-xl overflow-hidden shadow-lg">
+            <div className="h-36 overflow-hidden relative">
+              <img
+                src="/images/logo-llama.png"
+                alt="Llucky Llama"
+                className="w-full h-full object-contain"
+              />
+              <div className="absolute inset-0"></div>
+            </div>
+            <div className="p-6">
+              <h3 className="text-l font-semibold text-gray-900 mb-2">
+                Llucky Llama
+              </h3>
+              <p className="text-gray-600 mb-4 text-sm">
+                Randomizer web app that provides spin the bottle, wheel of
+                names, slotmachine, and pick a card features.
+              </p>
+              <a
+                href="https://github.com/elijah-alonzo/lucky-llama.git"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center gap-2 text-blue-500 hover:text-blue-600 text-sm"
