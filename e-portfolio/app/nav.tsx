4ef4118b39a39490b5d038cd8090967b55d30185
Elijah Alonzo <elijahalonzo.me@gmail.com>
2026-01-30 19:37:40 +0800

    update logo

--- e-portfolio/app/nav.tsx
+++ e-portfolio/app/nav.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center text-base">
         <div className="flex items-center">
           <img
-            src="/elijah-alonzo.png"
+            src="/sys-logo.png"
             alt="Elijah Alonzo"
             className="h-10 w-auto"
           />
